981afc6d47c01bdb6c864faa830f348aaefa4516
lj4 <136166458+4lj4@users.noreply.github.com>
2023-12-06 22:30:26 +0000

    make it hello world
    235 bytes

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 __attribute__((force_align_arg_pointer))
 void _start(){
-    asm("mov $1,%eax;"
+    asm (
+        "int $0x80;"
+        :
+        :"a"(4),"b"(1),"c"("helloworld"),"d"(11)
+    );
+    asm(
+        "mov $1,%eax;"
         "xor %ebx,%ebx;"
         "int  $0x80"
     );
