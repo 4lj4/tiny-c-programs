d35bd191e66f4539104aa25041500e4774b1f5d2
lj4 <136166458+4lj4@users.noreply.github.com>
2023-12-06 23:10:18 +0000

    Lets get funky with macros for the LOLs
    Still 235 bytes

--- main.c
+++ main.c
@@ -1,13 +1,8 @@
+#define asmWrite(str, len) asm("int $0x80;"::"a"(4),"b"(1),"c"(str),"d"(len))
+#define asmExit() asm("mov $1,%eax;xor %ebx,%ebx;int $0x80")
+
 __attribute__((force_align_arg_pointer))
 void _start(){
-    asm (
-        "int $0x80;"
-        :
-        :"a"(4),"b"(1),"c"("helloworld"),"d"(11)
-    );
-    asm(
-        "mov $1,%eax;"
-        "xor %ebx,%ebx;"
-        "int  $0x80"
-    );
+    asmWrite("helloworld", 11);
+    asmExit();
 }
